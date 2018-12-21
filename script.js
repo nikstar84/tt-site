eabbd041855478533ab544fe628b47839999031e
Niklas Scheicher <niklas.scheicher@gmail.com>
2018-12-21 13:13:29 +0100

    -commting previous work

---
+++ script.js
@@ -0,0 +1,19 @@
+function setBackground(id)
+{
+    id.style.background = "#ef7b00";
+    var x = id.getElementsByTagName("p");
+    for(i = 0; i<x.length; i++)
+    {
+        x[i].style.display = "block";
+    }
+}
+
+function resetBackground(id)
+{
+    id.style.background = "linear-gradient(90deg, #ef7b00 0%, #ef7b00 32%, #FFFFFF 100%)";
+    var x = id.getElementsByTagName("p");
+    for(i = 0; i<x.length; i++)
+    {
+        x[i].style.display = "none";
+    }
+}
