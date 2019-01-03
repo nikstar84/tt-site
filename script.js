3b1d2925c315afeba2be920f4509a4469e03004a
niklascheicher <niklas.scheicher@gmail.com>
2019-01-03 16:59:57 +0100

    added final content text

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ function setBackground(id)
 {
     id.style.background = "#ef7b00";
     var x = id.getElementsByTagName("p");
+    var y = id.getElementsByTagName("li");
     for(i = 0; i<x.length; i++)
     {
         x[i].style.display = "block";
+    }
+    for(i = 0; i<y.length; i++)
+    {
+        y[i].style.display = "list-item";
     }
 }
 
@@ -13,8 +18,13 @@ function resetBackground(id)
 {
     id.style.background = "linear-gradient(90deg, #ef7b00 0%, #ef7b00 32%, #FFFFFF 100%)";
     var x = id.getElementsByTagName("p");
+    var y = id.getElementsByTagName("li");
     for(i = 0; i<x.length; i++)
     {
         x[i].style.display = "none";
+    }
+    for(i = 0; i<y.length; i++)
+    {
+        y[i].style.display = "none";
     }
 }
